5018fd64803473b018d319a9b37da72336bc0eee
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2023-01-28 14:31:29 +0100

    add recommendations

--- web/src/Recommendations.js
+++ web/src/Recommendations.js
@@ -3,7 +3,8 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
-import { Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import { alignProperty } from '@mui/material/styles/cssUtils';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,9 +19,26 @@ export default function Recommendations(props) {
         <Stack spacing={2}>
             <Typography variant='h5'>Rekomendacje</Typography>
             {Object.entries(props.predictions).
-                map(([key, value]) => <p className="label"><b>{`${key}: ${value}`}</b></p>
+                map(([key, value]) =>
+                    <Card>
+                        <CardContent>
+                            <Typography textAlign='left'>{`Typ  : ${key}`}</Typography>
+                            <Typography textAlign='left'>{`Wartość: ${value}`}</Typography>
+                        </CardContent>
+                        <CardActions sx={{
+                            justifyContent: 'center',
+                        }}>
+                            <Button
+                                onClick={() => { alert("TODO: dodać obsługe sterownika") }}
+                                variant='outlined'
+                            >
+
+                                APLIKUJ
+                            </Button>
+                        </CardActions>
+                    </Card>
                 )
             }
-        </Stack>
+        </Stack >
     );
 }
