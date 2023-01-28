076f517cff4365759370027bd7f385351b3149cd
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2023-01-28 14:56:09 +0100

    switch dla rekomendacji

--- web/src/Recommendations.js
+++ web/src/Recommendations.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, FormControlLabel, Switch, Typography } from '@mui/material';
 import { alignProperty } from '@mui/material/styles/cssUtils';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -14,10 +14,71 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
+const IOSSwitch = styled((props) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+        padding: 0,
+        margin: 2,
+        transitionDuration: '300ms',
+        '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+                opacity: 1,
+                border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+                opacity: 0.5,
+            },
+        },
+        '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+        },
+        '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[600],
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        boxSizing: 'border-box',
+        width: 22,
+        height: 22,
+    },
+    '& .MuiSwitch-track': {
+        borderRadius: 26 / 2,
+        backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+        opacity: 1,
+        transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+        }),
+    },
+}));
+const SwitchXD = () => {
+    return (
+        <FormControlLabel
+            control={<IOSSwitch
+                sx={{ m: 1 }}
+                defaultChecked={false} />}
+            label="Automatyczna aplikacja"
+            disabled
+        />)
+}
+
 export default function Recommendations(props) {
     return (
         <Stack spacing={2}>
-            <Typography variant='h5'>Rekomendacje</Typography>
+            <Typography variant='h5'>Rekomendacje</ Typography> <SwitchXD />
             {Object.entries(props.predictions).
                 map(([key, value]) =>
                     <Card>
@@ -42,3 +103,5 @@ export default function Recommendations(props) {
         </Stack >
     );
 }
+
+
